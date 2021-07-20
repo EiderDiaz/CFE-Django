d9adb335ade6edd9aa64e89d118d3cdebf4cfeba
eiderdiazmoraila@hotail.com <eiderdiazmoraila@hotmail.com>
2021-07-20 18:59:06 -0500

    [deleted] migrations and DB, start from scratch new models

--- src/products/models.py
+++ src/products/models.py
@@ -6,6 +6,7 @@ class Product(models.Model):
     title = models.TextField()
     description = models.TextField()
     price = models.TextField()
+    summary = models.TextField(default = 'this is cool')
 
 
     #changes?
