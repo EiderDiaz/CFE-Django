17233a2502e0240d19ba3e807768f06675265f9b
eiderdiazmoraila@hotail.com <eiderdiazmoraila@hotmail.com>
2021-07-21 14:58:07 -0500

    models changed without deleting migrations or database -> default homepage to custom homepage

--- src/products/models.py
+++ src/products/models.py
@@ -7,6 +7,10 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=1000)
     summary = models.TextField()
+    # if i wanna add a new field and
+    #featured = models.BooleanField(null=True)  #set to null the old observations or default=False
+
+    featured = models.BooleanField()
 
 
     #changes?
